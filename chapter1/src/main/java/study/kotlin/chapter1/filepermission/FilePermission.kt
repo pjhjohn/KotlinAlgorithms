5b852c3e5dbdd80ec834fef5a5c63d67270c649d
JoonHo Park <pjhjohn@gmail.com>
2018-09-21 23:18:07 +0900

    Chapter 1 - FilePermission Symbol Converter
    UNIX File Permission Converter from Symbols to Octal Number
    - Read : `r`
    - Write : `w`
    - Execute : `x`
    - Unset : `-`
     
    You can try this on http://permissions-calculator.org/

---
+++ chapter1/src/main/java/study/kotlin/chapter1/filepermission/FilePermission.kt
@@ -0,0 +1,7 @@
+package study.kotlin.chapter1.filepermission
+
+object FilePermission {
+    fun symbolicToOctal(symbols: String): Int {
+        TODO("not implemented")
+    }
+}
---
+++ chapter1/src/test/java/study/kotlin/chapter1/filepermission/FilePermissionUnitTest.kt
@@ -0,0 +1,39 @@
+package study.kotlin.chapter1.filepermission
+
+import org.assertj.core.api.Assertions.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import study.kotlin.chapter1.UnitTestWatcher
+
+class FilePermissionUnitTest : UnitTestWatcher() {
+
+    @Before
+    fun before() {
+
+    }
+
+    @After
+    fun after() {
+
+    }
+
+    @Test
+    fun testFilePermission() {
+
+        assertThat(FilePermission.symbolicToOctal("rwxrwxrwx")).isEqualTo(777)
+        assertThat(FilePermission.symbolicToOctal("rwxrwx---")).isEqualTo(770)
+        assertThat(FilePermission.symbolicToOctal("rw-rw-rw-")).isEqualTo(666)
+        assertThat(FilePermission.symbolicToOctal("rw-----wx")).isEqualTo(603)
+        assertThat(FilePermission.symbolicToOctal("-wxr-xrw-")).isEqualTo(356)
+        assertThat(FilePermission.symbolicToOctal("rwxr-xr-x")).isEqualTo(755)
+        assertThat(FilePermission.symbolicToOctal("rw-r--r--")).isEqualTo(644)
+        assertThat(FilePermission.symbolicToOctal("---------")).isEqualTo(0)
+
+        assertThatIllegalArgumentException().isThrownBy { FilePermission.symbolicToOctal("") }
+        assertThatIllegalArgumentException().isThrownBy { FilePermission.symbolicToOctal("rwxrwxrw ") }
+        assertThatIllegalArgumentException().isThrownBy { FilePermission.symbolicToOctal("rwxrwxrwa") }
+        assertThatIllegalArgumentException().isThrownBy { FilePermission.symbolicToOctal("rwxrwxrwxrwx") }
+    }
+
+}
