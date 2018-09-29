be80de21eba073e6b359496195f085cda0fe653d
JoonHo Park <pjhjohn@gmail.com>
2018-09-29 16:57:04 +0900

    Implement FilePermission.symbolicToOctal

--- chapter1/src/main/java/study/kotlin/chapter1/filepermission/FilePermission.kt
+++ chapter1/src/main/java/study/kotlin/chapter1/filepermission/FilePermission.kt
@@ -1,7 +1,20 @@
 package study.kotlin.chapter1.filepermission
 
+import java.util.regex.Pattern
+
 object FilePermission {
+
+    private const val VALID_PATTERN = "[r-][w-][x-][r-][w-][x-][r-][w-][x-]"
+
     fun symbolicToOctal(symbols: String): Int {
-        TODO("not implemented")
+        if (!Pattern.compile(VALID_PATTERN).matcher(symbols).matches()) throw IllegalArgumentException("Malformed Permission String : $symbols")
+        return symbols.chunked(3).fold(0) { acc, chunk -> 10 * acc + parseUnitSymbols(chunk) }
     }
+
+    private fun parseUnitSymbols(symbols: String): Int {
+        if (symbols.length != 3) throw IllegalArgumentException("Malformed Unit Permission String : $symbols")
+        return symbols.zip("rwx").fold(0) { acc, (symbol, answer) -> 2 * acc + (symbol == answer).toInt() }
+    }
+
+    private fun Boolean.toInt(): Int = if (this) 1 else 0
 }
